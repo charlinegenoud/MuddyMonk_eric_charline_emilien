4beaea88ff6734c3c949925d3991faccff9f7990
ericmalick <113695592+ericmalick@users.noreply.github.com>
2023-01-18 11:08:26 +0100

    commit initialisation script

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ window.addEventListener("scroll", function() {
 
     square.style.transform = "translateY(" + 0.05 * val + "%" + ")";
 });
+
+var slideleft = document.querySelector(".textleft");
+var slideright = document.querySelector(".textright");
+
+// var scrolled;
+
+// window.addEventListener('scroll', function() {
+
+//     var value = window.scrollY;
+//     scrolled = window.pageYOffset;
+//     console.log(scrolled);
+
+// });
