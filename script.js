dcee89000058afaaf3acaeb8cb71a41600509e39
ericmalick <113695592+ericmalick@users.noreply.github.com>
2023-01-18 12:41:34 +0100

    rajout du burger menu et du logo d'intro

--- script.js
+++ script.js
@@ -20,6 +20,41 @@ window.addEventListener("scroll", function() {
 var slideleft = document.querySelector(".textleft");
 var slideright = document.querySelector(".textright");
 
+// -------------menu burger --------------
+
+const menu = document.querySelector(".toggle-nav");
+const main = document.querySelector("main");
+
+const burgerMenu = document.querySelector("#menu-burger");
+
+menu.addEventListener("click", function() {
+    main.classList.toggle("open");
+    if (main.classList.contains("open")) {
+        //menu.innerHTML = '<img src="assets/close.svg">Close';
+        burgerMenu.setAttribute("class", "clicked");
+    } else {
+        //menu.innerHTML = '<img src="assets/menu.svg">Menu';
+        burgerMenu.removeAttribute("class");
+    }
+});
+
+
+burgerMenu.addEventListener("click", function() {
+    main.classList.toggle("open");
+    if (main.classList.contains("open")) {
+        burgerMenu.setAttribute("class", "clicked");
+    } else {
+        burgerMenu.removeAttribute("class");
+    }
+});
+
+
+
+
+
+
+
+
 // var scrolled;
 
 // window.addEventListener('scroll', function() {
